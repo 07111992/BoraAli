18b27d0aafb527ca91d2f22e0caaf86d804fb19a
Eliel Santana <100521650+07111992@users.noreply.github.com>
2023-11-28 16:46:42 -0300

    alteraçao da tela de login

--- src/components/LoginCadastro/LoginCadastro.js
+++ src/components/LoginCadastro/LoginCadastro.js
@@ -1,97 +1,132 @@
-import { useState } from "react";
 import "../LoginCadastro/LoginCadastro.css";
-import { Link } from "react-router-dom";
 
 export default function LoginCadastro() {
-    const [isSignUpActive, setIsSignUpActive] = useState(false);
+  function Signin() {
+    var body = document.querySelector("body");
+    body.className = "sign-in-js";
+  }
 
-    return (
-        <div className="box-master">
-            <h2>BEM VINDO AMORZINHO</h2>
+  function Signup() {
+    var body = document.querySelector("body");
+    body.className = "sign-up-js";
+  }
 
-            <div
-                className={`container ${isSignUpActive ? "right-painel-active" : ""}`}
-            >
-                <div className="form-container sign-up-container">
-                    <form action="#">
-                        <h1>CRIAR CONTA</h1>
-
-                        <div className="social-container">
-                            <Link className="link" href="#" class="social">
-                                <i class="fab fa-facebook-f"></i>
-                            </Link>
-                            <Link className="link" href="#" class="social">
-                                <i class="fab fa-google-plus-g"></i>
-                            </Link>
-                            <Link className="link" href="#" class="social">
-                                <i class="fab fa-linkedin-in"></i>
-                            </Link>
-                        </div>
-                        <span>Use seu email para se cadastrar.</span>
-
-                        <input className="botao" type="text" placeholder="Nome" />
-                        <input className="botao" type="email" placeholder="Email" />
-                        <input className="botao" type="password" placeholder="Senha" />
-                        <button className="butao1" >CADASTRAR</button>
-                    </form>
-                </div>
-
-                <div className="form-container sign-in-container">
-                    <form action="#">
-                        <h1>ACESSO</h1>
-
-                        <div className="social-container">
-                            <Link className="link" href="#" class="social">
-                                <i class="fab fa-facebook-f"></i>
-                            </Link>
-                            <Link className="link" href="#" class="social">
-                                <i class="fab fa-google-plus-g"></i>
-                            </Link>
-                            <Link className="link" href="#" class="social">
-                                <i class="fab fa-linkedin-in"></i>
-                            </Link>
-                        </div>
-                        <span  >Acesso de usuário</span>
-
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Senha" />
-                        <Link className="link" href="#">ESQUECEU A SENHA?</Link>
-                        <button>ENTRAR</button>
-                    </form>
-                </div>
-
-                <div className="sobreposição-container">
-                    <div className="sobreposição">
-                        <div className="sobreposição-painel sobreposição-left">
-                            <h1>Se já for cadastrado, vem cá.</h1>
-                            <p>Informe seus dados de usuário.</p>
-                            <button
-                                class="ghost"
-                                id="SignIn"
-                                onClick={() => {
-                                    setIsSignUpActive(false);
-                                }}
-                            >
-                                Acessar
-                            </button>
-                        </div>
-
-                        <div className="sobreposição-painel sobreposição-right">
-                            <h1>Bora ali?</h1>
-                            <p>Por aqui pra se tornar um usuário.</p>
-                            <button
-                                className="ghost"
-                                id="SignUn"
-                                onClick={() => {
-                                    setIsSignUpActive(true);
-                                }}
-                            >
-                                Agora mesmo
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container">
+        <div className="content first-content">
+          <div className="first-column">
+            <h2 className="title title-primary">welcome back!</h2>
+            <p className="description description-primary">
+              To keep connected with us
+            </p>
+            <p className="description description-primary">
+              please login with your personal info
+            </p>
+            <button onClick={Signin} id="signin" className="btn btn-primary">
+              sign in
+            </button>
+          </div>
+          <div className="second-column">
+            <h2 className="title title-second">create account</h2>
+            <div className="social-media">
+              <ul className="list-social-media">
+                <a className="link-social-media" href="#">
+                  <li className="item-social-media">
+                    <i className="fab fa-facebook-f"></i>
+                  </li>
+                </a>
+                <a className="link-social-media" href="#">
+                  <li className="item-social-media">
+                    <i className="fab fa-google-plus-g"></i>
+                  </li>
+                </a>
+                <a className="link-social-media" href="#">
+                  <li className="item-social-media">
+                    <i className="fab fa-linkedin-in"></i>
+                  </li>
+                </a>
+              </ul>
             </div>
+            <p className="description description-second">
+              or use your email for registration:
+            </p>
+            <form className="form">
+              <label className="label-input" for="">
+                <i className="far fa-user icon-modify"></i>
+                <input type="text" placeholder="Name" />
+              </label>
+
+              <label className="label-input" for="">
+                <i className="far fa-envelope icon-modify"></i>
+                <input type="email" placeholder="Email" />
+              </label>
+
+              <label className="label-input" for="">
+                <i className="fas fa-lock icon-modify"></i>
+                <input type="password" placeholder="Password" />
+              </label>
+
+              <button className="btn btn-second">sign up</button>
+            </form>
+          </div>
         </div>
-    );
+        <div className="content second-content">
+          <div className="first-column">
+            <h2 className="title title-primary">hello, friend!</h2>
+            <p className="description description-primary">
+              Enter your personal details
+            </p>
+            <p className="description description-primary">
+              and start journey with us
+            </p>
+            <button onClick={Signup} id="signup" className="btn btn-primary">
+              sign up
+            </button>
+          </div>
+          <div className="second-column">
+            <h2 className="title title-second">sign in to developer</h2>
+            <div className="social-media">
+              <ul className="list-social-media">
+                <a className="link-social-media" href="#">
+                  <li className="item-social-media">
+                    <i className="fab fa-facebook-f"></i>
+                  </li>
+                </a>
+                <a className="link-social-media" href="#">
+                  <li className="item-social-media">
+                    <i className="fab fa-google-plus-g"></i>
+                  </li>
+                </a>
+                <a className="link-social-media" href="#">
+                  <li className="item-social-media">
+                    <i className="fab fa-linkedin-in"></i>
+                  </li>
+                </a>
+              </ul>
+            </div>
+            <p className="description description-second">
+              or use your email account:
+            </p>
+            <form className="form">
+              <label className="label-input" for="">
+                <i className="far fa-envelope icon-modify"></i>
+                <input type="email" placeholder="Email" />
+              </label>
+
+              <label className="label-input" for="">
+                <i className="fas fa-lock icon-modify"></i>
+                <input type="password" placeholder="Password" />
+              </label>
+
+              <a className="password" href="#">
+                forgot your password?
+              </a>
+              <button className="btn btn-second">sign in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
