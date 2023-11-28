4eb59b7a395ffadbcc6f4431607d55230e320af8
Eliel Santana <100521650+07111992@users.noreply.github.com>
2023-11-28 03:30:19 -0300

    finalizando o a pagina de quem somos

--- src/components/QuemSomos/quemSomos.js
+++ src/components/QuemSomos/quemSomos.js
@@ -1,62 +1,75 @@
-import './QuemSomos.css'
+import "./QuemSomos.css";
 
-import Isis from '../../img/isis.jpg'
-import Linkedin from '../../img/linkedin.png'
-import GitHub from '../../img/github_preto.png'
-import Italo from '../../img/foto_prof.jpg'
-import Douglas from '../../img/douglas.jpg'
-import Eliel from '../../img/foto eliel.jpg'
+import Isis from "../../img/isis.jpg";
+import Linkedin from "../../img/linkedinBlack.png";
+import GitHub from "../../img/gitBlack.png";
+import Italo from "../../img/foto_prof.jpg";
+import Douglas from "../../img/douglas.jpg";
+import Eliel from "../../img/foto eliel.jpg";
+import ItaloAtor from "../../img/italoAtor.jpg";
 
-export default function QuemSomos(){
-    return(
-        <main>    
-                            
-            <div className="container_participantes">
-                <div className="card_participantes" id="isis">
-                  
-                    <img className="img_participantes" src={Isis} id="isis"  alt="" />
-                    <p>Isis Braz</p>
-                    <div className="redes_sociais">
-                        <img src={Linkedin} alt=""/>
-                        <img src={GitHub} alt=""/>
-                    </div>
-                </div>
-                <div className="card_participantes">
-                    <img className="img_participantes" src={Italo} alt=""/>
-                    <p>Ithalo Soares</p>
-                    <div className="redes_sociais">
-                        <img src={Linkedin} alt=""/>
-                        <img src={GitHub} alt=""/>
-                    </div>
-                </div>
-                <div className="card_participantes">
-                <img className="img_participantes" src={Italo} alt=""/>
-                    <p>Gustavo Miranda</p>
-                    <div className="redes_sociais">
-                        <img src={Linkedin} alt=""/>
-                        <img src={GitHub} alt=""/>
-                    </div>
-                </div>
-                <div className="card_participantes">
-                <img className="img_participantes" src={Douglas} alt=""/>
-                    <p>Douglas Stevens</p>
-                    <div className="redes_sociais">
-                        <img src={Linkedin} alt=""/>
-                        <img src={GitHub} alt=""/>
-                    </div>
-                </div>
-                <div className="card_participantes">
-                <img className="img_participantes" src={Eliel} alt=""/>
-                    <p>Eliel Santana</p>
-                    <div className="redes_sociais">
-                        <img src={Linkedin} alt=""/>
-                        <img src={GitHub} alt=""/>
-                    </div>
-                </div>
+export default function QuemSomos() {
+  return (
+    <main>
+      <h1>Nossa Equipe Incrível</h1>
+
+      <div className="container_participantes">
+        <div className="card_participantes" id="isis">
+          <img className="img_participantes" src={Isis} id="isis" alt="" />
+          <div className="quadrado">
+            <p>Isis Braz</p>
+            <p className="dev">Desenvolvedor</p>
+            <div className="redes_sociais">
+              <img src={Linkedin} alt="" />
+              <img src={GitHub} alt="" />
             </div>
-        </main>   
-
-    )
-        
-    
+          </div>
+        </div>
+        <div className="card_participantes">
+          <img className="img_participantes" src={ItaloAtor} alt="" />
+          <div className="quadrado">
+            <p>Ithalo Soares</p>
+            <p className="dev">Desenvolvedor</p>
+            <div className="redes_sociais">
+              <img src={Linkedin} alt="" />
+              <img src={GitHub} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="card_participantes">
+          <img className="img_participantes" src={Italo} alt="" />
+          <div className="quadrado">
+            <p>Gustavo Miranda</p>
+            <p className="dev">Desenvolvedor</p>
+            <div className="redes_sociais">
+              <img src={Linkedin} alt="" />
+              <img src={GitHub} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="card_participantes">
+          <img className="img_participantes" src={Douglas} alt="" />
+          <div className="quadrado">
+            <p>Douglas Stevens</p>
+            <p className="dev">Desenvolvedor</p>
+            <div className="redes_sociais">
+              <img src={Linkedin} alt="" />
+              <img src={GitHub} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="card_participantes">
+          <img className="img_participantes" src={Eliel} alt="" />
+          <div className="quadrado">
+            <p>Eliel Santana</p>
+            <p className="dev">Desenvolvedor</p>
+            <div className="redes_sociais">
+              <img src={Linkedin} alt="" />
+              <img src={GitHub} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
